52ca6c85b51075fa05a383abffdb246b0602dd97
Enzo Belem <enzogt5000@gmail.com>
2022-09-22 02:15:58 -0300

    ajuste frontend cadastro de palavras

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,17 +1,17 @@
 const submit = document.getElementById("word_reg_submit");
 
-const word=  document.getElementById("word_input");
-const word_type=  document.getElementById("word_type_select");
-const word_lang=  document.getElementById("word_lang_input");
-const word_meaning=  document.getElementById("word_meaning_input");
+const word = document.getElementById("word_input");
+const word_type = document.getElementById("word_type_select");
+const word_lang = document.getElementById("word_lang_input");
+const word_meaning = document.getElementById("word_meaning_input");
 
 submit.addEventListener('click', async (e) => {
     e.preventDefault()
     try {
-        let response = await fetchUserSend('http://localhost:8080/word', 'POST', {
+        let response = await requestFactory('http://localhost:8080/word', 'POST', {
             'word': word.value,
             'type': word_type.options[word_type.selectedIndex].value,
-            'language':word_lang.value,
+            'language': word_lang.value,
             'meaning': word_meaning.value
         })
         console.log(response)
@@ -21,12 +21,21 @@ submit.addEventListener('click', async (e) => {
     document.querySelector("form").reset()
 })
 
-const fetchUserSend = async (url, method, data) => {
+const requestFactory = async (url, method, data) => {
     let dataReturn
-    let init = {
-        method: method,
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data)
+    let init
+
+    if (method === 'GET') {
+        init = {
+            method: method,
+            headers: { 'Content-Type': 'application/json' }
+        }
+    } else {
+        init = {
+            method: method,
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        }
     }
 
     await fetch(url, init)
@@ -35,3 +44,26 @@ const fetchUserSend = async (url, method, data) => {
 
     return dataReturn
 }
+
+const settingTypeOptions = async () => {
+    let response
+    let options = `<option> </option>`
+    let select = document.querySelector("#word_type_select")     
+
+    try{
+        response = await requestFactory("http://localhost:8080/word/types", 'GET')
+        if(response.length){
+            options=  Object.values(response).map(type => `<option value=${type}>${type}</option>`).join('')
+        }    
+    }catch(error){
+        console.error(error)
+    }
+
+    select.innerHTML= options
+}
+
+const initialization = async()=>{
+    await settingTypeOptions()
+}
+
+document.body.onload= initialization()
